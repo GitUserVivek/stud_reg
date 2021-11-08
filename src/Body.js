c59b3bf36f9b6eeed923f2b45bb002cbd6759e70
Jvs <jagtapvs9832@gmail.com>
2021-11-08 19:29:48 +0530

    user form completed with storing process

--- src/Body.js
+++ src/Body.js
@@ -1,5 +1,27 @@
+import { HashRouter as Router } from 'react-router-dom';
+import './Css/Body.css';
 export default function Body() {
-    return <div className="Body">
+    const redirect = () => { window.location.replace("http://www.w3schools.com"); }
 
-    </div>
+    return <Router><div className="Body">
+        <form action="https://mrunkn.000webhostapp.com/PHP/Stud_Reg/StoreData.php" method="POST">
+            <label>name        </label>  <input name='name' type="text"></input>
+            <label>dob         </label>  <input name='dob' type="date"></input>
+            <label>gender      </label>  <input name='gender' type="text"></input>
+            <label>email       </label>  <input name='email' type="text"></input>
+            <label>address     </label>  <input name='address' type="text"></input>
+            <label>department  </label>  <input name='department' type="text"></input>
+            <label>course      </label>  <input name='course' type="text"></input>
+            <label>mobile      </label>  <input name='mobile' type="number"></input>
+            <button type="submit" onClick={() => redirect}>Submit </button>
+        </form>
+    </div >
+    </Router >
 }
+
+
+
+
+
+
+
