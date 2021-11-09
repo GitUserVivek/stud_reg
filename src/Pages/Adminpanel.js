1d75d478a904414127072f25b7e2378a777b658c
Jvs <jagtapvs9832@gmail.com>
2021-11-09 20:36:39 +0530

    95% completed

--- src/Pages/Adminpanel.js
+++ src/Pages/Adminpanel.js
@@ -31,7 +31,12 @@ export default function Adminpanel() {
                     {
                         Data.map((index) => {
                             return search ? index["NAME"].includes(search) ? <tr key={index["SR"]}>
-                                <td>{index["SR"]}</td><td>{index["NAME"]}</td><td>{index["DEPARTMENT"]}</td><td>{index["COURSE"]}</td>
+                                <td>{index["SR"]}</td><td>{index["NAME"]}</td><td>{index["DEPARTMENT"]}</td><td>{index["COURSE"]}</td> <td className="operation"><form action="https://mrunkn.000webhostapp.com/PHP/Stud_Reg/StoreData.php" method="POST">
+                                    <input name='action' value="DeleteData" type="hidden"></input>
+                                    <input name='index' value={index["SR"]} type="hidden"></input>
+                                    <input type="SUBMIT"></input>
+
+                                </form></td>
                             </tr> : null : <tr key={index["SR"]}>
                                 <td>{index["SR"]}</td><td>{index["NAME"]}</td><td>{index["DEPARTMENT"]}</td><td>{index["COURSE"]}</td>
                                 <td className="operation"><form action="https://mrunkn.000webhostapp.com/PHP/Stud_Reg/StoreData.php" method="POST">
